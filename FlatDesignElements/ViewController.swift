683199d47704760fac9e98386a74b8cdd73358a1
Hassaan Raza <rhrh1997@gmail.com>
2017-04-11 16:08:08 -0500

    Initial Commit

---
+++ FlatDesignElements/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlatDesignElements
+//
+//  Created by RAZA on 4/11/17.
+//  Copyright © 2017 RAZA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
