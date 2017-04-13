bdedd540105d602b0f963cdae83bbf65f9257756
Hassaan Raza <rhrh1997@gmail.com>
2017-04-12 23:21:47 -0500

    Separated Custom TextView Class

--- FlatDesignElements/UIFloatingFieldView.swift
+++ FlatDesignElements/UIFloatingFieldView.swift
@@ -8,22 +8,127 @@
 
 import UIKit
 
-class UIFloatingFieldView: UIView {
+class UIFloatingFieldView: UITextField, UITextFieldDelegate {
     
     var underline:CAShapeLayer?
-    var textField: UITextField!
+    var placeholderText:String?
     
     override init (frame : CGRect)
     {
         super.init(frame : frame)
-        var field = UITextField(frame: frame)
-        textField = field
-        
+        delegate = self
+        commonInit()
         
     }
     
+    func setPlaceholder(text: String)
+    {
+        placeholderText = text
+        self.placeholder = placeholderText
+    }
+    
+    func commonInit()
+    {
+        self.borderStyle = UITextBorderStyle.none
+        self.addTarget(self, action:#selector(fieldShow(_:)), for: UIControlEvents.touchDown)
+        self.addTarget(self, action:#selector(fieldHide(_:)), for: .editingDidEnd)
+        let image = UIImage(named: "007-user")
+        setupField(fieldImage: image!)
+        changePlaceholder()
+    }
+    
+    func setupField(fieldImage: UIImage)
+    {
+        let fieldImage = fieldImage
+        let field_x = CGFloat(0)
+        let field_y = CGFloat(0)
+        let field_end = field_y + self.frame.height + 5
+        let field_size = self.frame.width
+        
+        //Add Image
+        let vwContainer = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 25))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 25, height: 25))
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        let image = fieldImage
+        imageView.image = image
+        vwContainer.addSubview(imageView)
+        self.leftViewMode = UITextFieldViewMode.always
+        self.leftView = vwContainer
+        
+        //Add Line
+        let line = CAShapeLayer()
+        let linePath = UIBezierPath()
+        linePath.move(to: CGPoint(x:field_x, y:field_end))
+        linePath.addLine(to: CGPoint(x:field_x + field_size, y:field_end))
+        line.path = linePath.cgPath
+        line.strokeColor = UIColor.white.cgColor
+        line.lineWidth = 1
+        line.lineJoin = kCALineJoinRound
+        underline = line
+        self.layer.addSublayer(underline!)
+
+    }
+    
+    
+    func changePlaceholder()
+    {
+        var placeHolder = NSMutableAttributedString()
+        
+        if(placeholderText == nil)
+        {
+            placeholderText = ""
+        }
+        
+        // Set the Font
+        placeHolder = NSMutableAttributedString(string:placeholderText!, attributes: [NSFontAttributeName:UIFont(name: "Helvetica", size: 15.0)!])
+        
+        // Set the color
+        placeHolder.addAttribute(NSForegroundColorAttributeName, value: UIColor.white, range:NSRange(location:0,length:placeholderText!.characters.count))
+        
+        // Add attribute
+        self.attributedPlaceholder = placeHolder
+        
+    }
+    
+    func fieldShow(_ sender:UITextField)
+    {
+        let animationDuration = 0.7
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(animationDuration)
+        CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut))
+        self.underline?.lineWidth = 2
+        CATransaction.commit()
+        UIView.animate(withDuration: animationDuration, delay: 0.0, options: .curveEaseOut
+            , animations: {
+                sender.alpha = 1.0
+        }
+            , completion:nil
+        )
+        
+    }
+    
+    func fieldHide(_ sender:UITextField)
+    {
+        let animationDuration = 0.7
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(animationDuration)
+        CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut))
+        self.underline?.lineWidth = 1
+        CATransaction.commit()
+        UIView.animate(withDuration: animationDuration, delay: 0.0, options: .curveEaseOut
+            , animations: {
+                sender.alpha = 1.0
+        }
+            , completion:nil
+        )
+    }
+    
+    
+
+    
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        commonInit()
     }
     
 
--- FlatDesignElements/ViewController.swift
+++ FlatDesignElements/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet var field: UITextField!
+    @IBOutlet var userView: UIFloatingFieldView!
     var underLine:CAShapeLayer?
     
     override func viewDidLoad() {
@@ -18,71 +18,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.darkGray
 
-        field.addTarget(self, action:#selector(fieldShow(_:)), for: UIControlEvents.touchDown)
-        field.addTarget(self, action:#selector(fieldHide(_:)), for: .editingDidEnd)
-        let image = UIImage(named: "007-user")
-        underLine = self.setupField(_field: field, fieldImage: image!)
-        self.view.layer.addSublayer(underLine!)
-        changePlaceholder()
-
-    }
-
-
-    
-    func changePlaceholder()
-    {
-        var placeHolder = NSMutableAttributedString()
-        let Name  = "Hello"
-
-        // Set the Font
-        placeHolder = NSMutableAttributedString(string:Name, attributes: [NSFontAttributeName:UIFont(name: "Helvetica", size: 15.0)!])
-        
-        // Set the color
-        placeHolder.addAttribute(NSForegroundColorAttributeName, value: UIColor.white, range:NSRange(location:0,length:Name.characters.count))
-        
-        // Add attribute
-        field.attributedPlaceholder = placeHolder
-        
     }
     
-    func fieldShow(_ sender:UITextField)
-    {
-        let animationDuration = 0.7
-        CATransaction.begin()
-        CATransaction.setAnimationDuration(animationDuration)
-        CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut))
-        self.underLine?.lineWidth = 2
-        CATransaction.commit()
-        UIView.animate(withDuration: animationDuration, delay: 0.0, options: .curveEaseOut
-            , animations: {
-                sender.alpha = 1.0
-        }
-            , completion:nil
-        )
-        
-    }
-    
-    func fieldHide(_ sender:UITextField)
-    {
-        let animationDuration = 0.7
-        CATransaction.begin()
-        CATransaction.setAnimationDuration(animationDuration)
-        CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut))
-        self.underLine?.lineWidth = 1
-        CATransaction.commit()
-        UIView.animate(withDuration: animationDuration, delay: 0.0, options: .curveEaseOut
-            , animations: {
-                self.field.alpha = 1.0
-        }
-            , completion:nil
-        )
-    }
-
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true) //This will hide the keyboard
+        self.view.endEditing(true)
     }
     
+
+
+    
+
+    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -90,39 +38,4 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
 }
 
-
-extension UIViewController {
-    
-    func setupField(_field: UITextField, fieldImage: UIImage) -> CAShapeLayer
-    {
-        let fieldImage = fieldImage
-        let field_x = _field.frame.origin.x
-        let field_y = _field.frame.origin.y
-        let field_end = field_y + _field.frame.height + 5
-        let field_size = _field.frame.width
-        
-        //Add Image
-        let vwContainer = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 25))
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 25, height: 25))
-        imageView.contentMode = UIViewContentMode.scaleAspectFill
-        let image = fieldImage
-        imageView.image = image
-        vwContainer.addSubview(imageView)
-        _field.leftViewMode = UITextFieldViewMode.always
-        _field.leftView = vwContainer
-    
-        //Add Line
-        let line = CAShapeLayer()
-        let linePath = UIBezierPath()
-        linePath.move(to: CGPoint(x:field_x, y:field_end))
-        linePath.addLine(to: CGPoint(x:field_x + field_size, y:field_end))
-        line.path = linePath.cgPath
-        line.strokeColor = UIColor.white.cgColor
-        line.lineWidth = 1
-        line.lineJoin = kCALineJoinRound
-        return line
-    }
-    
-    
-}
 
